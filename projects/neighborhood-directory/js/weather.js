2bc76ea2a279fdf3e43434a48458cfe89f65416b
PhilippeWorks <philippe_t@live.ca>
2017-11-10 08:43:13 -0500

    fixed xml request bug

--- projects/neighborhood-directory/js/weather.js
+++ projects/neighborhood-directory/js/weather.js
@@ -4,7 +4,7 @@ let weatherElement = document.createElement("h2");
 
 //Requests weather data from openweathermap API
 let weatherRequest = new XMLHttpRequest();
-weatherRequest.open('GET', 'http://api.openweathermap.org/data/2.5/weather?lat=44.193342&lon=-79.9344491&APPID=f40a647622b63f285f4935a615ee62c4&units=metric');
+weatherRequest.open('GET', 'https://api.openweathermap.org/data/2.5/weather?lat=44.193342&lon=-79.9344491&APPID=f40a647622b63f285f4935a615ee62c4&units=metric');
 weatherRequest.onload = () => {
 	weather = JSON.parse(weatherRequest.response);
 };
