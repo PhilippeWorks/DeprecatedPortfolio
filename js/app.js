9201f6eb8e64e8c08415bce0cd38884384409730
PhilippeWorks <philippe_t@live.ca>
2017-11-11 18:57:57 -0500

    slideshow fix attempt #1

--- js/app.js
+++ js/app.js
@@ -87,22 +87,21 @@ class navButton {
 			navBar.forEach( (element) => {
 
 				if (element.button != this.button || this.page.css("display") != "flex") {
-					element.page.fadeOut(100);
-									navBar.forEach( (element) => {
-					element.page.css("display", "none");
-				})	
+					element.page.fadeOut(400);
+										
 				}
 			})
 
 			window.setTimeout( () => {
 				//prevents loading of multiple pages on repeated fast clicks
-
-				//shows requested page
-				this.page.css("display", "flex");
-				this.page.fadeIn(400);
-				
-				
+					navBar.forEach( (element) => {
+					element.page.css("display", "none");
+				})
 			}, 400)
+
+			//shows requested page
+			this.page.css("display", "flex");
+			this.page.fadeIn(400);
 			}
 		});
 
